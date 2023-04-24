a416b03c188b73cc401240eb5b9190a5a12da85c
Wiwi <buzdugan.victor@icloud.com>
2023-04-24 23:53:27 +0300

    organizing and add multiple words

--- main.py
+++ main.py
@@ -4,10 +4,21 @@ from classes.WordDictionary import WordDictionary
 
 
 base_dir = PurePosixPath(__file__).parent
-path = Path(base_dir, 'input', 'DEXOnline_small.xml')
-word_dictionary = WordDictionary(path)
+path = Path(base_dir, 'input', 'DEXOnline_xsmall.xml')
+dictionary = WordDictionary(path)
 
-word_dictionary.export_to_file(base_dir)
+# word_dictionary.export_to_file(base_dir)
 
 # variables
-# actions like '--- Saving dictionary to file ---'
+# actions like "... Loading xml file ..."
+
+
+
+x = input("Options: ('q' for quit): ")
+
+if x == "q":
+    dictionary.save_xml()
+
+
+
+
